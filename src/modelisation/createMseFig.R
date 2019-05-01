2093b3533c3d57bc0e314dc1ffdf976e6bcc5e25
samperochkin <samuel.perreault.3@ulaval.ca>
2019-05-01 03:05:42 -0400

    first half looking pretty good

--- src/modelisation/createMseFig.R
+++ src/modelisation/createMseFig.R
@@ -1,16 +1,20 @@
 createMseFig <- function(path = "static-files/"){
   
-  x0 <- .75
-  Ex0 <- dnorm(x0,1,1/2)
-  
+  x0 <- c(.75,1.25)
+  Sig <- matrix(1/2^2,2,2) # la matrice de variance-covariance pour la classe 1
+  Sig[2,2] <- Sig[2,2] + 1/4^2 # la variance de x2
+  Ex0 <- dmvnorm(x0, mean = c(1,2), sigma = Sig)/(dmvnorm(x0, mean = c(0,0), sigma = Sig) + dmvnorm(x0, mean = c(1,2), sigma = Sig)) # la prob conditionnelle que x0 = 1
+
   preds <- sapply(1:50, function(k){
+    print(k)
     
-    replicate(2000, {
+    replicate(1000, {
       train_dummy <- data.table(y = sample(0:1, 50, replace = T))
-      train_dummy[, x := rnorm(n=50, mean=y, sd=1/2)]
+      train_dummy[, x1 := rnorm(n=50, mean=y, sd=1/2)]
+      train_dummy[, x2 := x1 + rnorm(n=50, mean=y, sd=1/4)]
       
-      new_y_pred <- class::knn(train = matrix(train_dummy$x,ncol=1),
-                               test = matrix(x0,ncol=1),
+      new_y_pred <- class::knn(train = matrix(c(train_dummy$x1,train_dummy$x2),ncol=2),
+                               test = matrix(x0,ncol=2),
                                cl = train_dummy$y,
                                k = k)
       as.numeric(as.vector(new_y_pred))
