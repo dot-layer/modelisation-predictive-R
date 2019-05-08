e714bcaa134867e9acb11e8e3aca663d6f687347
LCULaval <laurent.m.caron@gmail.com>
2019-05-08 18:00:35 -0400

    Déplacer le formattage des données dans merge_data

--- src/extraction/load-historical-data.R
+++ src/extraction/load-historical-data.R
@@ -17,11 +17,5 @@ load_historical_data <- function(path_save_data) {
     data_bixi <- data.table::fread(paste0(path_save_data, "data_bixi.csv"))
   }
   
-  data_bixi[, `:=`(start_date_time = as.POSIXct(lubridate::fast_strptime(start_date, format = "%Y-%m-%d %H:%M")),
-                   end_date_time = as.POSIXct(lubridate::fast_strptime(end_date, format = "%Y-%m-%d %H:%M")))]
-  
-  data_bixi[, `:=`(start_date = as.Date(start_date_time),
-                   end_date = as.Date(end_date_time))]
-  
   data_bixi
 }
--- src/extraction/merge-data.R
+++ src/extraction/merge-data.R
@@ -1,5 +1,11 @@
 merge_data <- function(data_bixi, data_stations, points_stations)
 {
+  data_bixi[, `:=`(start_date_time = as.POSIXct(lubridate::fast_strptime(start_date, format = "%Y-%m-%d %H:%M")),
+                   end_date_time = as.POSIXct(lubridate::fast_strptime(end_date, format = "%Y-%m-%d %H:%M")))]
+  
+  data_bixi[, `:=`(start_date = as.Date(start_date_time),
+                   end_date = as.Date(end_date_time))]
+  
   data_stations[, quartier := points_stations$quartier]
   setkey(data_stations, code)
   setkey(data_bixi, start_station_code)
