04e9380f3c88cf27d8052a24b0cdffa7a621ee56
LCULaval <laurent.m.caron@gmail.com>
2019-05-08 22:17:59 -0400

    Load shapefile from AWS

--- src/collecte/load-merging-data.R
+++ src/collecte/load-merging-data.R
@@ -1,15 +1,31 @@
 load_merging_data <- function(path_save_data)
 {
-  if (!file.exists(paste0(path_save_data, "data_stations.csv"))){
-    data_stations <- fread("https://s3.ca-central-1.amazonaws.com/jeremiedb/share/dot-layer/R-Quebec/BixiMontrealRentals2017/Stations_2017.csv")
-    fwrite(data_stations, paste0(path_save_data, "data_stations.csv"))
+  AWS_URL <- "https://s3.ca-central-1.amazonaws.com/jeremiedb/share/dot-layer/R-Quebec"
+  
+  # Merger de l'info sur les stations
+  path_data_stations <- paste0(path_save_data, "data_stations.csv")
+  
+  if (!file.exists(path_data_stations)){
+    data_stations <- fread(paste0(AWS_URL, "/BixiMontrealRentals2017/Stations_2017.csv"))
+    fwrite(data_stations, path_data_stations)
   } else {
-    data_stations <- fread(paste0(path_save_data, "data_stations.csv"))
+    data_stations <- fread(path_data_stations)
   }
   
   # Merger de l'info sur les quartiers
-  path_geo <- paste0(path_save_data, "LIMADMIN.shp")
-  shape_file <- read_sf(dsn=path.expand(path_geo))
+  path_geo <- paste0(path_save_data, "LIMADMIN")
+  
+  if (!file.exists(path_geo)){
+    purrr::walk(c(".shx", ".shp", ".prj", ".dbf"),
+                ~ download.file(url = paste0(AWS_URL, "/LIMADMIN/LIMADMIN", .x),
+                                destfile = paste0(path_geo, .x),
+                                method = "auto",
+                                mode = "wb"))
+    shape_file <- read_sf(dsn=path.expand(paste0(path_geo, ".shp")))
+    write_sf(shape_file, path_geo)
+  } else {
+    shape_file <- read_sf(dsn=path.expand(paste0(path_geo, ".shp")))
+  }
   
   points_stations <- data.frame(
     x = data_stations$longitude,
