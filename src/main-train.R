f3b17b2d592558cf9ba6c0d790fbb2e6836e6d51
samperochkin <samuel.perreault.3@ulaval.ca>
2019-05-12 16:57:06 -0400

    creation données test et update section

--- src/main-train.R
+++ src/main-train.R
@@ -58,20 +58,6 @@ preprocessed_objects <- preprocessing_main(data_bixi[-ind_test,], train_mode = T
 
 
 
-# Création test -----------------------------------------------------------
-preprocessed_objects_test <- preprocessing_main(data_bixi[ind_test,], train_mode = TRUE)
-
-# X_regression_test <- copy(preprocessed_objects_test$data_regression)[, target_duree := NULL]
-# X_classif_test <- copy(preprocessed_objects_test$data_classif)[, target_meme_station := NULL]
-# y_regression_test <- preprocessed_objects_test$data_regression$target_duree
-# y_classif_test <- preprocessed_objects_test$data_classif$target_meme_station
-# 
-# write.fst(X_regression_test, "src/modelisation/X_regression_test.fst")
-# saveRDS(y_regression_test, "src/modelisation/y_regression_test.rds")
-# write.fst(X_classif_test, "src/modelisation/X_classif_test.fst")
-# saveRDS(y_classif_test, "src/modelisation/y_classif_test.rds")
-
-
 # Saver les objets
 # Objects communs au 2 modeles
 write(jsonlite::toJSON(preprocessed_objects$variables_a_imputer, pretty = TRUE), paste0(path_objects, "valeurs_imputations.json"))
@@ -89,10 +75,25 @@ y_regression <- preprocessed_objects$data_regression$target_duree
 y_classif <- preprocessed_objects$data_classif$target_meme_station
 
 
+# Sauvegarde pour section modelisation
 # write.fst(X_regression, "src/modelisation/X_regression.fst")
 # write.fst(X_classif, "src/modelisation/X_classif.fst")
 # saveRDS(y_regression, "src/modelisation/y_regression.rds")
 # saveRDS(y_classif, "src/modelisation/y_classif.rds")
+
+# y_regression_test <- data_bixi[ind_test, duration_sec]
+# y_classif_test <- as.numeric(data_bixi[ind_test, start_station_code] == data_bixi[ind_test, end_station_code])
+# 
+# preprocessed_objects_test <- preprocessing_main(data_bixi[ind_test,], train_mode = FALSE,
+#                                                 list_objects = preprocessed_objects)
+# X_regression_test <- copy(preprocessed_objects_test$data_regression)
+# X_classif_test <- copy(preprocessed_objects_test$data_classif)
+# 
+# write.fst(X_regression_test, "src/modelisation/X_regression_test.fst")
+# saveRDS(y_regression_test, "src/modelisation/y_regression_test.rds")
+# write.fst(X_classif_test, "src/modelisation/X_classif_test.fst")
+# saveRDS(y_classif_test, "src/modelisation/y_classif_test.rds")
+
 
 # Modeling ----------------------------------------------------------------
 
